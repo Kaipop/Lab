c2e876621d9a9c7dc156cf70caad792aadf9b62a
Kaipop <ken6622345@gmail.com>
2022-05-31 21:49:50 +0800

    註冊完成

---
+++ WebApplication1/WebApplication1/Repository/ProductRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models.DBEntity;
+using WebApplication1.Repository.Interface;
+
+namespace WebApplication1.Repository
+{
+    public class ProductRepository : DBRepository, IProductRepositroy
+    {
+        public ProductRepository(TPE21_Winter_BuildSchool_LabContext dbContext) : base(dbContext)
+        {
+
+        }
+        //客製的方法//針對資料庫的操作
+        public void DeleteById(int id)
+        {
+            var target = DBContext.Products.First(x => x.Id == id);
+            Delete<Product>(target);
+        }
+    }
+}
